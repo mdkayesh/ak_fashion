beadb9d8f44da7f4c7b3152ac13aa1b6b09fbb80
KayesKhanAkash <kayeskhanakash@gmail.com>
2023-08-18 00:06:59 +0600

    set some env variables

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -12,13 +12,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCJgK7J-vMwstBBJEJiD8BDlTnc9vx-WOM",
-  authDomain: "ak-fashion-cf12f.firebaseapp.com",
-  projectId: "ak-fashion-cf12f",
-  storageBucket: "ak-fashion-cf12f.appspot.com",
-  messagingSenderId: "6625886960",
-  appId: "1:6625886960:web:7a748de006b815e2833835",
-  measurementId: "G-HWW5K1ERS9",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
